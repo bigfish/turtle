61041da0dd60fd9f7f69438a149685cdd2bdeeb7
David Wilhelm <dewilhelm@gmail.com>
2010-08-01 20:32:57 -0400

    added some more functions

--- turtle_procedures.js
+++ turtle_procedures.js
@@ -28,5 +28,59 @@ function HOUSE(size) {
     TRIANGLE(size);
 }
 
+function THING() {
+    FORWARD(100);
+    RIGHT(90);
+    FORWARD(100);
+    RIGHT(90);
+    FORWARD(50);
+    RIGHT(90);
+    FORWARD(50);
+    RIGHT(90);
+    FORWARD(100);
+    RIGHT(90);
+    FORWARD(25);
+    RIGHT(90);
+    FORWARD(25);
+    RIGHT(90);
+    FORWARD(50);
+}
+
+function CIRCLE(size) {
+    REPEAT(360, function () {
+        FORWARD(size);
+        RIGHT(1);
+    });
+}
+
+function ARCR(r, deg) {
+    REPEAT(deg, function () {
+        FORWARD(r);
+        RIGHT(1);
+    });
+}
+
+function ARCL(r, deg) {
+    REPEAT(deg, function () {
+        FORWARD(r);
+        LEFT(1);
+    });
+}
+
+function PETAL(size) {
+    ARCR(size, 60);
+    RIGHT(120);
+    ARCR(size, 60);
+    RIGHT(120);
+}
+
+function FLOWER(size) {
+    REPEAT(6, function () {
+        PETAL(size);
+        RIGHT(60);
+    });
+}
+
+
 
 
