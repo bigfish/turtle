ad5340394055fb0afadd46df59fe076c9e2cd83d
David Wilhelm <dewilhelm@gmail.com>
2010-08-15 16:50:40 -0400

    added explorer files, made look nicer IMHO

---
+++ demos/turtle/js/turtle_global.js
@@ -0,0 +1,72 @@
+//Turtle graphics in global scope
+//(uses global functions for easier interactive use)
+//but only suports a single Turtle context
+//for OOP style use turtle.js & turtle_procedures.js
+
+var CTX, CANVAS_WIDTH, CANVAS_HEIGHT, BG, FG;
+
+function RESET() {
+    //clear state
+    CTX.restore();
+    //save initial state for later restore
+    CTX.save();
+    //delete everything
+    CTX.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
+    //fill with background color
+    CTX.fillRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
+ 
+    //move to center of canvas
+    CTX.translate(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+}
+
+function INIT(canvas, bg, fg) {
+    
+    if (typeof canvas === "string") {
+        canvas = document.getElementById(canvas);
+    }
+    if (canvas.getContext) {
+        CTX = canvas.getContext("2d");
+    } else {
+        return;
+    }
+
+    CANVAS_WIDTH = canvas.width;
+    CANVAS_HEIGHT = canvas.height;
+
+    BG = bg || "#000000";
+    FG = fg || "#00FF00";
+
+    CTX.fillStyle = BG;
+    CTX.strokeStyle = FG;
+
+    CTX.save();
+    RESET();
+}
+
+function FORWARD(len) {
+    CTX.beginPath();
+    CTX.moveTo(0, 0);
+    //forwards = up : reverse the Y coordinate
+    CTX.lineTo(0, - len);
+    CTX.translate(0, - len);
+    CTX.stroke();
+}
+
+function BACK(len) {
+    FORWARD( - len);
+}
+
+function RIGHT(angle) {
+    CTX.rotate(angle * Math.PI / 180);
+}
+
+function LEFT(angle) {
+    RIGHT( - angle);
+}
+
+function REPEAT(times, fn, args) {
+    for (var i = 0; i < times; i++) {
+        fn.apply(this, args || []);
+    }
+}
+
---
+++ demos/turtle_explorer/snippets/circles.js
@@ -0,0 +1,15 @@
+FORWARD(size);
+RIGHT(1);
+function CIRCLE(size) {
+    REPEAT(360, function () {
+        FORWARD(size);
+        RIGHT(1);
+    });
+}
+//examples
+CIRCLE(1);
+CIRCLE(2);
+CIRCLE(3);
+CIRCLE(4);
+RIGHT(5);
+//click RUN repeatedly ;)
